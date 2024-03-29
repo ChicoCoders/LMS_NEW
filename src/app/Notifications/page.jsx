b2ed95c2079e0ccc2e84e20408954533b42d2119
Nipuni31 <anuradhagangodawilann@gmail.com>
2024-03-29 18:59:19 +0530

    removed

--- src/app/Notifications/page.jsx
+++ src/app/Notifications/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navigations from '../Component/Navigations'
 import { HomeOutlined,MessageOutlined } from '@ant-design/icons';
 import { Button, Card } from 'antd';
-// import NotificationTable from './Component/NotificationTable'
+import NotificationTable from './Component/NotificationTable'
 
 
 const PageRoot1 = [
