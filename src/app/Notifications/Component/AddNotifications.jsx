5412899bd64cb5291bc3caeb23db2de55e372d72
ushaeni <139000191+ushaeni@users.noreply.github.com>
2024-03-31 20:49:57 +0530

    branch updated

--- src/app/Notifications/Component/AddNotifications.jsx
+++ src/app/Notifications/Component/AddNotifications.jsx
@@ -4,7 +4,8 @@ const { Option } = Select;
 
 const AddNotification = ({ visible, onCreate, onCancel }) => {
   const [form] = Form.useForm();
-
+  const[respient,setrecepient]=useState("student")
+  console.log(respient)
   return (
     <Modal
       visible={visible}
@@ -26,16 +27,23 @@ const AddNotification = ({ visible, onCreate, onCancel }) => {
     >
       <Form form={form} layout="vertical" name="form_in_modal">
         <Form.Item name="to" label="To" rules={[{ required: true, message: 'Please select the recipient!' }]}>
-          <Select placeholder="Select Recipient">
-            <Option value="All">All</Option>
-            <Option value="Faculty">Faculty</Option>
-            <Option value="Students">Students</Option>
+          <Select placeholder="Select Recipient" onSelect={(value)=>setrecepient(value)} >
+            
+            <Option value="all">All</Option>
+            <Option value="admin">Admin</Option>
+            <Option value="patron">Patrons</Option>
+            <Option value="other">Others</Option>
             {/* Add more options based on your notification recipients */}
           </Select>
         </Form.Item>
-        <Form.Item name="type" label="Type" rules={[{ required: true, message: 'Please select the notification type!' }]}>
-          <Input placeholder="Enter Type" />
+        {
+          (respient=="other")?
+          <Form.Item name="userId" label="User Name" rules={[{ required: true, message: 'Please select username!' }]}>
+          <Input placeholder="Enter User Name" />
         </Form.Item>
+        :<div></div>
+        }
+        
         <Form.Item name="description" label="Description" rules={[{ required: true, message: 'Please enter the notification description!' }]}>
           <Input.TextArea rows={4} placeholder="Enter Description" />
         </Form.Item>
--- src/app/Notifications/Component/RemindNotification.jsx
+++ src/app/Notifications/Component/RemindNotification.jsx
@@ -1,16 +1,17 @@
 // RemindNotification.jsx
-import React from 'react';
-import { Modal, Form, Select, DatePicker, TimePicker, Button, Input } from 'antd';  // Add 'Input' to the import statement
+import React, { useState } from 'react';
+import { Modal, Form, Select, DatePicker, TimePicker, Button, Input, Switch } from 'antd';  // Add 'Input' to the import statement
 const { Option } = Select;
 
 const RemindNotification = ({ visible, onRemind, onCancel }) => {
     const [form] = Form.useForm();
-
+    const[enable,setEnable]=useState(true);
+    console.log(enable);
     return (
         <Modal
             visible={visible}
             title="Remind Notification"
-            okText="Remind"
+            okText="Setup"
             cancelText="Cancel"
             onCancel={onCancel}
             onOk={() => {
@@ -20,28 +21,18 @@ const RemindNotification = ({ visible, onRemind, onCancel }) => {
                 });
             }}
         >
+            
             <Form form={form} layout="vertical" name="form_in_modal">
-                <Form.Item name="to" label="To" rules={[{ required: true, message: 'Please enter the recipient!' }]}>
-                    {/*<Select placeholder="Select Recipient">*/}
-                    {/*    <Option value="All">All</Option>*/}
-                    {/*    <Option value="Faculty">Faculty</Option>*/}
-                    {/*    <Option value="Students">Students</Option>*/}
-                    {/*    /!* Add more options based on your notification recipients *!/*/}
-                    {/*</Select>*/}
-                    <Input placeholder="Enter recipient" />
-                </Form.Item>
-                <Form.Item name="type" label="Type" rules={[{ required: true, message: 'Please select the notification type!' }]}>
-                    <Input placeholder="Enter Type" />
+            <Switch defaultChecked  onChange ={ (checked) => setEnable(checked)}/>
+            <br></br>
+            <br></br>
+                <Form.Item  name="subject" label="Subject" rules={[{ required: true, message: 'Please set a subject' }]}>
+                    <Input disabled={!enable} placeholder="Enter Type" />
                 </Form.Item>
                 <Form.Item name="description" label="Description" rules={[{ required: true, message: 'Please enter the notification description!' }]}>
-                    <Input.TextArea rows={4} placeholder="Enter Description" />
+                    <Input.TextArea disabled={!enable}  rows={4} placeholder="Enter Description" />
                 </Form.Item>
-                <Form.Item name="date" label="Date" rules={[{ required: true, message: 'Please select the date!' }]}>
-                    <DatePicker />
-                </Form.Item>
-                <Form.Item name="time" label="Time" rules={[{ required: true, message: 'Please select the time!' }]}>
-                    <TimePicker />
-                </Form.Item>
+                
             </Form>
         </Modal>
     );
